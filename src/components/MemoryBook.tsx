1316ffc9f62c1747c80ca99cd97563e2990ed3de
yardenyo <gmloler@gmail.com>
2024-01-17 19:03:05 +0200

    changes

--- src/components/MemoryBook.tsx
+++ src/components/MemoryBook.tsx
@@ -1,43 +1,58 @@
 import Testimonial from "@/components/Testimonial";
+import { Carousel } from "primereact/carousel";
+import { useState } from "react";
 
 const MemoryBook = () => {
+  const [testimonials] = useState([
+    {
+      name: "מיכל גבאי",
+      title: "אמה של שני",
+      quote:
+        "שני חיה את החיים בדיוק כפי שחלמה ורצתה. שני חיה לפי הסלוגן הקבוע שלה - 'No time for drama'. זו היתה דרך החיים שלה. לא עניין אותה כמה כסף יש לה בבנק, או מתי צריך ללכת לישון כי קמים מוקדם לעבודה.\n\n" +
+        "את שני עניין נטו דבר אחד, להנות בחיים. היא ניצלה כל דקה, טסה לכל מקום שרצתה, יצאה לבלות ללא הפסקה, הלכה לים בכל סופש ועשתה פשוט מה שבא לה.\n\n" +
+        "כמה שזה היה נכון, ואיזה מזל שהיא עשתה את זה. תמיד חייה על הקצה, חוותה כל רגע בדיוק כמו שרצתה ולא הגבילה את עצמה בגלל אחרים, כאילו ידעה תמיד שהחיים כל כך קצרים וצריך לנצל כל רגע.\n\n" +
+        "בגדול, שני השיגה כל מה שרצתה בחיים. היא חלמה מגיל קטן להיות עורכת דין והשיגה זאת בהצטיינות. חלמה לטוס לפסטיבלים בעולם והיתה בטומורולנד כבר לפני צבא ובעוד מאות מסיבות ופסטיבלים בארץ ובעולם. חלמה לעבור לגור באזור המרכז והספיקה לגור עם שותפות כבר בכמה דירות בתל אביב.",
+      image:
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705167590/DSC_7045_g2lpgd.jpg",
+    },
+    {
+      name: "ניצן גבאי",
+      title: "אחותה של שני",
+      quote:
+        "אחותי\nאני אוהבת אותך\nאני מתגעגעת אלייך\nמבטיחה לספר לכולם עלייך\nהמלאכית הכי מיוחדת בגן עדן👼🏼",
+      image:
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705510185/DSC_6788_Original_b1i2vi.jpg",
+    },
+    {
+      name: "אביאל גבאי",
+      title: "אחיה של שני",
+      quote:
+        "להיות אחיך הגדול\nהיתה המתנה הכי גדולה שקיבלתי בחיי\nמתגעגע אלייך כל כך 💔",
+      image:
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705510330/NI1_1736_qlj4i2.jpg",
+    },
+  ]);
+
   return (
     <div className="border-t border-black border-opacity-5">
       <div className="flex flex-col py-8 container mx-auto">
         <div className="title text-center text-3xl font-semibold">
           ספר הזכרונות
         </div>
-        <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <Testimonial
-            name="מיכל גבאי"
-            title="אמה של שני"
-            quote="אנחנו מתגעגעים אלייך כל כך
-            הלב שורף, המחשבות לא עוזבות לדקה
-            קשה להאמין ובלתי אפשרי לקלוט
-            שאת לא פה.
-            אוהבים אותך מלאכית שלנו
-            תמיד איתנו בלב❤️‍🩹"
-            image="https://res.cloudinary.com/dweltcoxk/image/upload/v1705167590/DSC_7045_g2lpgd.jpg"
-          />
-          <Testimonial
-            name="ניצן גבאי"
-            title="אחותה של שני"
-            quote="אחותי
-            אני אוהבת אותך
-            אני מתגעגעת אלייך
-            מבטיחה לספר לכולם עלייך
-            המלאכית הכי מיוחדת בגן עדן👼🏼"
-            image="https://res.cloudinary.com/dweltcoxk/image/upload/v1705166541/NI1_1349_lq0yec.jpg"
-          />
-          <Testimonial
-            name="אביאל גבאי"
-            title="אחיה של שני"
-            quote="להיות אחיך הגדול
-            היתה המתנה הכי גדולה שקיבלתי בחיי
-            מתגעגע אלייך כל כך 💔"
-            image="https://res.cloudinary.com/dweltcoxk/image/upload/v1705166655/NI1_1743_lwzadz.jpg"
-          />
-        </div>
+        <Carousel
+          value={testimonials}
+          numScroll={1}
+          className="mt-8"
+          itemTemplate={(testimonial) => (
+            <Testimonial
+              name={testimonial.name}
+              title={testimonial.title}
+              quote={testimonial.quote}
+              image={testimonial.image}
+            />
+          )}
+          dir="ltr"
+        ></Carousel>
       </div>
     </div>
   );
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -7,27 +7,29 @@ type Props = {
 
 const Testimonial = ({ name, title, quote, image }: Props) => {
   return (
-    <div className="relative p-4 flex border gap-4 h-[350px] overflow-hidden hover:scale-105 transition duration-500 ease-in-out">
+    <div
+      className="relative p-4 flex border gap-4 h-[350px] overflow-hidden rounded-lg"
+      dir="rtl"
+    >
       <div className="absolute -bottom-5 -left-5 w-32 h-32 bg-gradient-to-br from-[#FF0000] to-[#FF7878] rounded-full blur-3xl opacity-75"></div>
 
-      <div className="w-1/2">
+      <div className="flex-shrink-0 w-1/3">
         <img
           src={image}
           alt=""
-          className="w-full h-full object-cover border rounded-lg"
+          className="max-w-full h-full object-center border rounded-lg"
         />
       </div>
 
-      <div className="relative w-1/2 flex flex-col justify-center">
-        <div className="absolute top-0 text-5xl lg:text-7xl bg-gradient-to-r from-[#FF0000] to-[#FF7878] inline-block text-transparent bg-clip-text font-bold">
+      <div className="flex flex-col flex-grow">
+        <div className="text-5xl lg:text-7xl bg-gradient-to-r from-[#FF0000] to-[#FF7878] inline-block text-transparent bg-clip-text font-bold">
           ”
         </div>
 
-        <div className="flex flex-col mt-4">
+        <div className="flex flex-col gap-2">
           <div className="text-xl font-semibold">{name}</div>
           <div className="text-lg font-semibold">{title}</div>
           <div className="text-base">{quote}</div>
-          <button className="btn btn-primary mt-4 w-1/2">קראו עוד</button>
         </div>
       </div>
     </div>
