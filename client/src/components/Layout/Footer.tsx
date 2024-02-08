6b4d3c152b766f6e8867eb0bee562e8888d7e1a0
yardenyo <gmloler@gmail.com>
2024-02-09 01:21:34 +0200

    changes

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -7,16 +7,16 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-gray-100/20 container mx-auto flex justify-between py-8">
+    <footer className="bg-gray-100/20 container mx-auto flex flex-col lg:flex-row justify-between py-8">
       <div className="logo-section">
         <Link smooth to="/">
-          <div className="text-2xl font-semibold navbar-link">
+          <div className="text-2xl font-semibold navbar-link text-center lg:text-right">
             דף הבית של <span className="text-red-500">שני</span>
           </div>
         </Link>
       </div>
-      <div className="links-section flex-col">
-        <div className="flex gap-4">
+      <div className="links-section flex-col p-4 lg:p-0">
+        <div className="flex flex-col text-center lg:flex-row lg:text-right gap-4">
           {NavbarLinks.map((link) => (
             <div key={link.key} className="navbar-link">
               <Link smooth to={link.path}>
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="social-media-section">
-        <div className="flex items-center gap-4">
+        <div className="flex justify-center items-center gap-4">
           <a
             href="https://www.facebook.com/remember.shani.gabay"
             target="_blank"
