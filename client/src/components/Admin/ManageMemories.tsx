02f95817d0023c8ac702a80c5ba0e085be3bf944
yardenyo <gmloler@gmail.com>
2024-02-09 00:16:01 +0200

    changes

--- client/src/components/Admin/ManageMemories.tsx
+++ client/src/components/Admin/ManageMemories.tsx
@@ -6,7 +6,7 @@ import { IMemory } from "@/types/memories";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import useToast from "@/hooks/useToast";
-import { confirmDialog, ConfirmDialog } from "primereact/confirmdialog";
+import { confirmDialog } from "primereact/confirmdialog";
 import { useState } from "react";
 import MemoryDialog from "@/components/Dialogs/MemoryDialog";
 
