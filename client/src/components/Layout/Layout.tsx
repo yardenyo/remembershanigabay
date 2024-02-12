8e3ff03a2f27fee30399de012325655bc31faf80
yardenyo <gmloler@gmail.com>
2024-02-12 13:09:35 +0200

    changes

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -3,17 +3,26 @@ import BackgroundSound from "@/components/Layout/BackgroundSound";
 import ToTop from "@/components/Layout/ToTop";
 import NoTimeForDrama from "@/components/Layout/NoTimeForDrama";
 import Footer from "@/components/Layout/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { animateScroll as scroll } from "react-scroll";
 
 const Layout = () => {
   const [isPlaying, setIsPlaying] = useState(true);
+  const { pathname } = useLocation();
 
   useEffect(() => {
     setTimeout(() => {
       setIsPlaying(false);
     }, 3000);
   }, []);
+
+  useEffect(() => {
+    scroll.scrollToTop({
+      duration: 500,
+      smooth: "easeInOutQuad",
+    });
+  }, [pathname]);
 
   return (
     <main className="font-body relative overflow-x-hidden">
