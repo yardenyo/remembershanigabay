5df512f11c4ed556251ad6635dd499f312a566e3
yardenyo <gmloler@gmail.com>
2024-02-14 20:26:00 +0200

    changes

--- client/src/views/BlackSaturday.tsx
+++ client/src/views/BlackSaturday.tsx
@@ -10,18 +10,20 @@ const BlackSaturday = () => {
   }) => {
     return (
       <div
-        className="p-8 border rounded-lg shadow-lg bg-white flex flex-col gap-4"
+        className="p-4 border rounded-lg shadow-lg text-white flex flex-col gap-4 bg-gradient-to-r from-black to-black/90"
         dir="rtl"
       >
-        <div className="wrapper">
-          <div className="text-2xl font-semibold text-red-500">
-            {event.title}
+        <div className="p-4 isolate w-full rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
+          <div className="wrapper">
+            <div className="text-2xl font-semibold text-red-500">
+              {event.title}
+            </div>
+            <div className="text-lg">{event.place}</div>
           </div>
-          <div className="text-lg">{event.place}</div>
-        </div>
-        <div className="wrapper">
-          <div className="text-lg">{event.time}</div>
-          <div className="text-lg">{event.description}</div>
+          <div className="wrapper">
+            <div className="text-lg">{event.time}</div>
+            <div className="text-lg">{event.description}</div>
+          </div>
         </div>
       </div>
     );
@@ -41,6 +43,7 @@ const BlackSaturday = () => {
           השבת השחורה
         </div>
         <div className="title-underline" />
+        <div className="flex justify-center text-xl">07/10/2023</div>
         <div className="hidden lg:flex flex-col-reverse lg:flex-row-reverse justify-center items-center p-8 gap-4">
           <Timeline
             value={BlackSaturdayEvents}
@@ -52,22 +55,31 @@ const BlackSaturday = () => {
           />
         </div>
         <div className="justify-center lg:hidden">
-          <div className="p-8 flex flex-col gap-4" dir="rtl">
+          <div className="p-2 flex flex-col gap-4" dir="rtl">
             {BlackSaturdayEvents.map((event, index) => (
-              <div
-                key={index}
-                className="border rounded-lg shadow-lg bg-white flex flex-col gap-4 p-4"
-              >
-                <div className="wrapper">
-                  <div className="text-2xl font-semibold text-red-500">
-                    {event.title}
+              <div key={index}>
+                <div
+                  key={index}
+                  className="border rounded-lg shadow-lg bg-black text-white flex flex-col gap-4 p-4"
+                >
+                  <div className="p-4 isolate w-full rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
+                    <div className="wrapper">
+                      <div className="text-2xl font-semibold text-red-500">
+                        {event.title}
+                      </div>
+                      <div className="text-lg">{event.place}</div>
+                    </div>
+                    <div className="wrapper">
+                      <div className="text-lg">{event.time}</div>
+                      <div className="text-lg">{event.description}</div>
+                    </div>
                   </div>
-                  <div className="text-lg">{event.place}</div>
                 </div>
-                <div className="wrapper">
-                  <div className="text-lg">{event.time}</div>
-                  <div className="text-lg">{event.description}</div>
-                </div>
+                {index !== BlackSaturdayEvents.length - 1 && (
+                  <div className="custom-marker p-2 flex justify-center items-center">
+                    <i className="pi pi-arrow-down text-red-500" />
+                  </div>
+                )}
               </div>
             ))}
           </div>
