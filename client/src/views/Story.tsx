49d17bb077ef1b46452b1fd875a77f5c42337211
yardenyo <gmloler@gmail.com>
2024-02-14 15:22:10 +0200

    changes

--- client/src/views/Story.tsx
+++ client/src/views/Story.tsx
@@ -4,19 +4,19 @@ const Story = () => {
       year: "1997",
       text: "שני נולדה ב29/11/1997 בבית החולים בני ציון בחיפה",
       image:
-        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705159244/IMG_20180222_093118_405_aubfet.jpg",
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1706727359/c05ei0xlmrqhd5klqxc2.jpg",
     },
     {
       year: "2003",
       text: "ניתוח עיניים ראשון להצלת הראייה (הראשונה בארץ)",
       image:
-        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705159244/IMG_20180222_093118_405_aubfet.jpg",
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1707074096/ct4v9vktqn2ysnlaulq3.jpg",
     },
     {
       year: "2004",
       text: "שני נכנסת לבית ספר יסודי",
       image:
-        "https://res.cloudinary.com/dweltcoxk/image/upload/v1705159244/IMG_20180222_093118_405_aubfet.jpg",
+        "https://res.cloudinary.com/dweltcoxk/image/upload/v1706727309/odze765de6wpndneisvf.jpg",
     },
   ];
 
@@ -28,21 +28,21 @@ const Story = () => {
         </div>
         <div className="title-underline" />
         <div className="timeline-wrapper flex gap-12 justify-center py-8 lg:py-20">
-          <div className="">
+          <div>
             <div className="timeline">
               {timeLine.map((item, index) => {
                 return (
                   <div key={index} className="timeline-item">
-                    <div className="timeline-item-image h-[500px] flex gap-4 justify-center">
+                    <div className="timeline-item-image h-[400px] w-[600px] flex gap-4 justify-center">
                       {index % 2 === 0 ? (
                         <img
                           src={item.image}
                           alt="shani"
-                          className="p-4 hover:scale-105 transition duration-500 ease-in-out"
+                          className="p-4 h-[400px] object-cover hover:scale-105 transition duration-500 ease-in-out"
                         />
                       ) : (
                         <div className="flex flex-col items-end justify-center gap-4 w-full">
-                          <h2 className="text-5xl text-red-500">{item.year}</h2>
+                          <h2 className="text-4xl text-red-500">{item.year}</h2>
                           <p className="text-xl text-gray-400">{item.text}</p>
                         </div>
                       )}
@@ -59,10 +59,10 @@ const Story = () => {
                 {timeLine.map((item, index) => {
                   return (
                     <div key={index} className="timeline-item">
-                      <div className="timeline-item-image h-[500px] flex gap-4 justify-center">
+                      <div className="timeline-item-image h-[400px] w-[600px] flex gap-4 justify-center">
                         {index % 2 === 0 ? (
                           <div className="flex flex-col items-start justify-center gap-4 w-full">
-                            <h2 className="text-5xl text-red-500">
+                            <h2 className="text-4xl text-red-500">
                               {item.year}
                             </h2>
                             <p className="text-xl text-gray-500">{item.text}</p>
@@ -71,7 +71,7 @@ const Story = () => {
                           <img
                             src={item.image}
                             alt="shani"
-                            className="p-4 hover:scale-105 transition duration-500 ease-in-out"
+                            className="p-4 h-[400px] object-cover hover:scale-105 transition duration-500 ease-in-out"
                           />
                         )}
                       </div>
