658004d9064715d3b6262adc3a05a5c6362b70fd
yardenyo <gmloler@gmail.com>
2024-02-16 18:53:51 +0200

    changes

--- client/src/components/Layout/BackgroundSound.tsx
+++ client/src/components/Layout/BackgroundSound.tsx
@@ -1,50 +1,35 @@
-import { useState, useEffect } from "react";
-import { Tooltip } from "primereact/tooltip";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
 const BackgroundSound = () => {
-  const [isDivVisible, setDivVisible] = useState(true);
+  const [isVideoPlaying, setVideoPlaying] = useState(false);
 
-  const toggleDivVisibility = () => {
-    setDivVisible(!isDivVisible);
+  const toggleVideoPlaying = () => {
+    setVideoPlaying(!isVideoPlaying);
   };
-
-  useEffect(() => {
-    const youtubeIcon = document.querySelector(".pi-youtube");
-    if (youtubeIcon) {
-      youtubeIcon.dispatchEvent(new Event("mouseenter"));
-    }
-  }, []);
 
   return (
     <div>
-      <div
-        className={
-          isDivVisible
-            ? "fixed bottom-14 right-5 z-50 border border-black border-opacity-10 p-4"
-            : "hidden"
-        }
-      >
+      {isVideoPlaying && (
         <iframe
-          className="w-full lg:w-[550px] h-full lg:h-[300px]"
-          src="https://www.youtube.com/embed/MnQz0P4F30I?autoplay=1&mute=1&loop=1"
+          className="w-0 h-0"
+          src={`https://www.youtube.com/embed/MnQz0P4F30I?&loop=1&autoplay=${
+            isVideoPlaying ? 1 : 0
+          }`}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
           frameBorder="0"
           allowFullScreen
           loading="lazy"
         ></iframe>
-      </div>
+      )}
 
-      <i
-        onClick={toggleDivVisibility}
-        className={`pi pi-youtube fixed bottom-2 right-5 z-50 text-5xl ${
-          isDivVisible ? "text-red-500 hover:text-red-600" : "text-black"
-        } transition duration-500 ease-in-out cursor-pointer`}
-      ></i>
-      <Tooltip
-        target=".pi-youtube"
-        position="left"
-        content={isDivVisible ? "לחץ כדי להסתיר" : "לחץ כדי להציג"}
+      <FontAwesomeIcon
+        icon={!isVideoPlaying ? faVolumeXmark : faVolumeUp}
+        onClick={toggleVideoPlaying}
+        className="fixed bottom-3 right-6 z-50 text-4xl transition duration-500 ease-in-out cursor-pointer"
       />
     </div>
   );
